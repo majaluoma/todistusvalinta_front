dc9358f0915558508ed6f4a685914fbc53a830ca
majaluoma <olli.majaluoma@gmail.com>
2024-12-28 20:15:45 +0200

    Footer style modifications

--- src/pages/root/Footer.tsx
+++ src/pages/root/Footer.tsx
@@ -7,40 +7,29 @@ import privacyPolicy from '@/assets/privacyPolicy.pdf';
 export default function Footer() {
   return (
     <footer className="bg-muted text-muted-foreground mt-auto w-full shadow-lg shadow-secondary">
-      <div className="container mx-auto px-4 py-8 ">
-        <div className="flex flex-col md:flex-row justify-between  mb-8 ">
-          <div className="mb-4 md:mb-0 flex ">
-            <h2 className="text-2xl font-bold text-primary">Todistusvalinta.fi</h2>
-          </div>
-          <nav className="flex flex-wrap md:justify-end gap-4">
-            <a href={privacyPolicy} className="text-blue-900 underline text-base">
-              Tietosuojaseloste
-            </a>
-          </nav>
-        </div>
+      <div className="container mx-auto px-4 py-8 flex flex-col items-center justify-center gap-4">
+        <h2 className="text-2xl font-bold text-primary">Todistusvalinta.fi</h2>
 
-        <div className="flex flex-col md:flex-row justify-between ">
-          <div className="text-start md:text-left mb-4 md:mb-0">
-            <h3 className="font-semibold mb-2">Yhteystiedot</h3>
+        <nav className="flex flex-wrap gap-4">
+          <a href={privacyPolicy} className="text-blue-900 underline text-base">
+            Tietosuojaseloste
+          </a>
+        </nav>
 
-            <p className="flex flex-row align-baseline">
-              Sähköposti:{' '}
-              <img
-                src={"/assets/contactEmailImage.png"}
-                alt="sahkopostiosoite"
-                className="align-baseline ml-2 w-auto h-max relative "
-              />
-            </p>
-          </div>
-        </div>
-
-        <div className="mt-8 text-start text-sm">
-          <p>
-            &copy; {new Date().getFullYear()} Todistusvalinta.fi All
-            rights reserved.
-          </p>
-        </div>
+        <p className="flex flex-row align-baseline">
+          Sähköposti:{' '}
+          <img
+            src={'/assets/contactEmailImage.png'}
+            alt="sahkopostiosoite"
+            className="align-baseline ml-2 w-auto h-max relative "
+          />
+        </p>
+      <p>
+        &copy; {new Date().getFullYear()} Todistusvalinta.fi All rights
+        reserved.
+      </p>
       </div>
+
     </footer>
   );
 }
