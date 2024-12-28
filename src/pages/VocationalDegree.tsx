5e1c361447d47e948bdf78965d31f5f7fd38e4d8
majaluoma <olli.majaluoma@gmail.com>
2024-12-28 07:17:08 +0200

    Added degree scales for vocational claclualtor

--- src/pages/VocationalDegree.tsx
+++ src/pages/VocationalDegree.tsx
@@ -1,44 +1,71 @@
-import { Switch } from "@/components/ui/switch";
-import Calculator from "@/features/calculator/Calculator";
-import { useState } from "react";
+import { Label } from '@/components/ui/label';
+import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
+import Calculator from '@/features/calculator/Calculator';
+import { useState } from 'react';
 
 export default function VocationalDegree() {
-  const [degreeScaleIs5, setDegreeStateIs5] = useState<boolean>(true);
-  const optionType5 = [{
-    type: "ammMalu1-5",
-  },
-  {
-    type: "ammYhty1-5",
-  },
-  {
-    type: "ammVivu1-5",
-  },
-  {
-    type: "ammKa1-5",
-  }]
-  const optionType3 = [{
-    type: "ammMalu1-3",
-  },
-  {
-    type: "ammYhty1-3",
-  },
-  {
-    type: "ammVivu1-3",
-  },
-  {
-    type: "ammKa1-3",
-  }]
+  const [degreeScale, setDegreeScale] = useState<"1-3" | "1-5">("1-5");
+
+  const handleChange = (value : string) => {
+    if (value === "1-3" || value === "1-5") {
+      setDegreeScale(value);
+    }
+  }
+
+  const optionType5 = [
+    {
+      type: 'ammMalu1-5',
+    },
+    {
+      type: 'ammYhty1-5',
+    },
+    {
+      type: 'ammVivu1-5',
+    },
+    {
+      type: 'ammKa1-5',
+    },
+  ];
+  const optionType3 = [
+    {
+      type: 'ammMalu1-3',
+    },
+    {
+      type: 'ammYhty1-3',
+    },
+    {
+      type: 'ammVivu1-3',
+    },
+    {
+      type: 'ammKa1-3',
+    },
+  ];
   return (
     <>
-    <div>
-    <Switch checked={degreeScaleIs5} onCheckedChange={setDegreeStateIs5}/>
-
-    </div>
-    {degreeScaleIs5 ? 
-    <Calculator optionTypes={optionType5} addableOptions={false} vocational = {true}/>
-    :
-    <Calculator optionTypes={optionType3} addableOptions={false} vocational = {true}/>}
+      <RadioGroup defaultValue="1-5" onValueChange={handleChange} className='flex flex-row gap-6 mb-4'  >
+        <p>Valitse todistuksesi arviointiasteikko:</p>
+        <div className="flex items-center space-x-2">
+          <RadioGroupItem value="1-5" id="1-5" />
+          <Label htmlFor="1-5">1-5</Label>
+        </div>
+        <div className="flex items-center space-x-2">
+          <RadioGroupItem value="1-3" id="1-3"/>
+          <Label htmlFor="1-3">1-3</Label>
+        </div>
+      </RadioGroup>
+      {degreeScale === "1-5" ? (
+        <Calculator
+          optionTypes={optionType5}
+          addableOptions={false}
+          vocational={true}
+        />
+      ) : (
+        <Calculator
+          optionTypes={optionType3}
+          addableOptions={false}
+          vocational={true}
+        />
+      )}
     </>
-
   );
 }
