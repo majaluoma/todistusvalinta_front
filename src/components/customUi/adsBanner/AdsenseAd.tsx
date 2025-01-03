2cc8884c1fe72592a29a17e4ef0abba12e314c3b
majaluoma <olli.majaluoma@gmail.com>
2025-01-03 18:31:00 +0200

    apparently loading is needed many times

--- src/components/customUi/adsBanner/AdsenseAd.tsx
+++ src/components/customUi/adsBanner/AdsenseAd.tsx
@@ -4,6 +4,18 @@ const VITE_ADSENSE_SLOT = import.meta.env.VITE_ADSENSE_SLOT;
 export default function AdsenseAd() {
   
   useEffect(() => {
+     // Ensure the script is loaded only once
+     const scriptId = 'adsbygoogle-script';
+     if (!document.getElementById(scriptId)) {
+       const script = document.createElement('script');
+       script.id = scriptId;
+       script.async = true;
+       script.src =
+         'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js';
+       script.crossOrigin="anonymous"
+       document.body.appendChild(script);
+     }
+     
     if (window.adsbygoogle) {
       try {
         window.adsbygoogle.push({});
