c8512529c21e7a5c87927f22e129c53902b24d1b
majaluoma <olli.majaluoma@gmail.com>
2025-01-07 06:52:58 +0200

    changed right calculationModels

--- src/App.tsx
+++ src/App.tsx
@@ -50,7 +50,7 @@ function App() {
         },
         {
           path: '/yo-laskuri',
-          element: <VocationalDegree />,
+          element: <MatriculationExamination />,
         },
         {
           path: '/ammatillinen-laskuri',
