05a2bdd8b0b1bde5ec73c197c7968133af35ee71
majaluoma <olli.majaluoma@gmail.com>
2025-01-09 20:54:07 +0200

    search icon animation

--- src/features/calculator/components/themeAccordion/SearchBar.tsx
+++ src/features/calculator/components/themeAccordion/SearchBar.tsx
@@ -18,15 +18,15 @@ export default function Searchbar({
   };
 
   return (
-    <div className="flex w-full mr-4 justify-between">
+    <div className="relative flex w-full mr-12 justify-between">
       <Input
         onInput={handleInput}
         type="search"
         placeholder="Etsi hakukohdetta..."
-        className="bg-input placeholder:text-sm md:text-base lg:text-base"
+        className="peer bg-input placeholder:text-sm md:text-base lg:text-base"
       ></Input>
       <img
-        className="relative size-5 right-8 top-2.5"
+        className="absolute size-5 right-3 top-2.5 peer-placeholder-shown:block peer-focus:hidden"
         src={searchIcon}
         alt={'etsi'}
       />
