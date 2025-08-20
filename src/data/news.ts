69d4c38f323a5b9545007095ed20656d4e49b777
majaluoma <olli.majaluoma@gmail.com>
2025-08-20 14:40:55 +0300

    news updated

--- src/data/news.ts
+++ src/data/news.ts
@@ -1,1 +1,1 @@
-export const mainNews = "Sivustolle on päivitetty kevään 2025 pisterajat. Päivitykseen myötä sivustolla esiintyy katkoksia, asiaa korjataan."
+export const mainNews = "Sivustolle on päivitetty kevään 2025 ja syksyn 2024 pisterajat. Varasijat on huomioitu tuloksissa."
