18392c01c97f93c86b3944a2bb2b782b0101fd93
Olli Majaluoma <olli.majaluoma@gmail.com>
2025-12-09 16:03:35 +0200

    Rename and update news constants

--- src/data/news.tsx
+++ src/data/news.tsx
@@ -1,6 +1,6 @@
-export const main2News: React.ReactNode = (
+export const mainNews: React.ReactNode = (
   <>
-    Yliopistojen todistusvalinnan laskumallit muuttuvat 2026, lue lisää {' '}
+    Yliopistojen todistusvalintaan on tulossa muutoksia 2026, lue lisää {' '}
     <a
       href="/reform2026"
       className="underline text-blue-500"
@@ -12,7 +12,7 @@ export const main2News: React.ReactNode = (
   </>
 );
 
-export const mainNews: React.ReactNode = (
+export const oldNews: React.ReactNode = (
   <>
     Sivustolle on päivitetty kevään 2025 ja syksyn 2024 pisterajat. Varasijat on huomioitu tuloksissa.
   </>
