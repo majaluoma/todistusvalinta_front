e03b9897bc6df4dfef5c70014c15c02cb7387842
majaluoma <olli.majaluoma@gmail.com>
2026-05-25 06:54:46 +0300

    uutisen päivitys

--- src/data/news.tsx
+++ src/data/news.tsx
@@ -7,7 +7,7 @@ export const oldnews2: React.ReactNode = (
 
 export const mainNews: React.ReactNode = (
   <>
-    Kevään 2026 pisterajat päivitetään sivustolle kesäkuun alussa, palaa myöhemmin uudestaan
+    Kevään 2026 alustavat pisterajat julkaistaan sivustolla 29.5. mennessä.
   </>
 );
 
